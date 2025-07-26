f9d506f4a3be8baa21c9b20d07aaa09695f78688
crittersmusic <thecrittersmusic@gmail.com>
2025-07-26 13:02:31 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
+from fastapi import FastAPI, UploadFile, File
+from app.processor import extract_tags
 
+app = FastAPI()
+
+@app.post("/analyze")
+async def analyze(file: UploadFile = File(...)):
+    contents = await file.read()
+
+    with open("temp.wav", "wb") as f:
+        f.write(contents)
+
+    tags = extract_tags("temp.wav")
+    return tags
